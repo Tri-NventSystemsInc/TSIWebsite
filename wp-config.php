abea46bdde232ce4fea4c8566b49d988ec8de8be
arjayads <arjayads@gmail.com>
2014-07-12 01:16:25 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'tsi');
+define('DB_NAME', 'db497486654');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'dbo497486654');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '$*FIJe45o3)#*(%');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db497486654.db.1and1.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
