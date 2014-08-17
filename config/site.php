958f0d53d63d74ca8345c8a55c47f99494ceac4f
106559 <u74321459@false.(none)>
2014-08-17 16:04:05 -0400

    site settings

--- config/site.php
+++ config/site.php
@@ -1,5 +1,6 @@
 <?php
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'db497486654.db.1and1.com');
+-define('DB_USERNAME', 'dbo497486654');
+-define('DB_PASSWORD', '$*FIJe45o3)#*(%');
 define('DB_DATABASE', 'db497486654');
+
