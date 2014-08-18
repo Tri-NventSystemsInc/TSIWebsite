265a2accfb551586831c7cbedc1e21734819207b
Arjay Adong <arjay.adong@datasouth.co.uk>
2014-08-18 14:49:01 +0100

    spot layout

---
+++ themes/spot/default.php
@@ -0,0 +1,16 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$this->inc('elements/header.php'); ?>
+
+	<div id="main-content-inner">
+		
+			<?php   
+			$a = new Area('Main');
+			$a->display($c); 
+			?> 
+			
+	</div>
+
+<?php    
+$this->inc('elements/footer.php');  
+?> 
