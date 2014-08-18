2ce618ab3b0d027880cb4c65e2eb5207d0027820
Arjay Adong <arjay.adong@datasouth.co.uk>
2014-08-18 15:50:00 +0100

    headers

--- themes/spot/default.php
+++ themes/spot/default.php
@@ -12,5 +12,11 @@ $this->inc('elements/header.php'); ?>
 	</div>
 
 <?php    
+
+$html = Loader::helper('html'); 
+$this->addFooterItem($html->javascript($this->getThemePath().'/assets/js/bootstrap.min.js'));    
+$this->addFooterItem($html->javascript($this->getThemePath().'/assets/js/Chart.js'));   
+
 $this->inc('elements/footer.php');  
+
 ?> 
