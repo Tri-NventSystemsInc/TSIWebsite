93cd19b34672757d66cc69d1b0585880aa950e71
Arjay Adong <arjay.adong@datasouth.co.uk>
2014-08-21 16:57:31 +0100

    favicon

--- themes/spot/elements/header.php
+++ themes/spot/elements/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="<?= $this->getThemePath() ?>/assets/img/ja.png">
+    <link rel="shortcut icon" href="<?= $this->getThemePath() ?>/assets/img/t.png">
 
     <!-- Bootstrap core CSS -->
     <link href="<?= $this->getThemePath() ?>/assets/css/bootstrap.css" rel="stylesheet">
