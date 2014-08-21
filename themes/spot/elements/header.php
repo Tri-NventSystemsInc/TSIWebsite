b6fc505391af3f6ab3c635eebf585a0f472dc831
Arjay Adong <arjayads@gmail.com>
2014-08-21 13:24:07 +0800

    page types

--- themes/spot/elements/header.php
+++ themes/spot/elements/header.php
@@ -1,61 +1,44 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
-
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="<?php echo LANGUAGE?>" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+<?php Loader::element('header_required'); ?>
+	
 
     <!-- Bootstrap core CSS -->
-    <link href="<?= $this->getThemePath() ?>/assets/css/bootstrap.css" rel="stylesheet">
-    <link href="<?= $this->getThemePath() ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('/assets/css/bootstrap.css')?>" />
+    <link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('/assets/css/font-awesome.min.css')?>" /> 
 
-    <!-- FONTS -->
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
-    <link href="http://fonts.googleapis.com/css?family=Raleway:300,400,700,900" rel="stylesheet">
+    <!-- Custom styles for this template --> 
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,900' rel='stylesheet' type='text/css'> 
+    <link rel="stylesheet" media="screen" type="text/css" href="<?php echo $this->getStyleSheet('/assets/css/main.css')?>" /> 
 
-    <!-- Custom styles for this template -->
-    <link href="<?= $this->getThemePath() ?>/assets/css/main.css" rel="stylesheet">
-
-    <?php Loader::element('header_required'); ?>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
+
+
 </head>
-
 <body>
-<?php
-    $user = new User();
-
-?>
 
 <!-- Fixed navbar -->
-<div class="navbar navbar-inverse <?php echo ($user->isSuperUser() && $user->isLoggedIn()) ? '' : 'navbar-fixed-top'?>">
-    <div class="container">
+    <div class="navbar navbar-inverse">
+      <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/"><span style="font-size: 12px;">This is a </span>SP<i class="fa fa-circle"></i>T <span style="font-size: 12px;">template</span></a>
-
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">tri-nvent</a>
         </div>
-        <div class="navbar-collapse collapse">
-            <div id="nav-list" class="hidden">
-                <?php
-                $a = new GlobalArea('Navigation');
-                $a->display();
-                ?>
-
-            </div>
+        <div class="navbar-collapse collapse"> 
         </div><!--/.nav-collapse -->
+      </div>
     </div>
-</div>
