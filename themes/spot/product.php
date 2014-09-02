63071f84eb7c3507b47ef9af715f42a98d471f75
Arjay Adong <arjayads@gmail.com>
2014-09-02 12:41:23 +0800

    update product page

--- themes/spot/product.php
+++ themes/spot/product.php
@@ -28,67 +28,67 @@ $this->inc('elements/header.php'); ?>
 				<p class="product_desc">
                     Complete Accounting with Purchasing, Inventory and Warehousing System - a four-in-one integrated system design for various businesses with especial design for cost segmentation/allocation
 				</p>
-			</div><!-- col-lg-4 -->
 
-			<div class="col-lg-4">
-				<i class="fa fa-cogs"></i>
-				<h4>Human Resource & Payroll System</h4>
-				<p class="product_desc">
-                   The system is deployed to company users with enable modules according to their uses. The software let everybody keep tracks of their individual records like leaves, absences, attendance, overtime, leave balances, etc.  They can process overtime, leave, failure to logs, etc. online without disturbing HR personnel.
-				</p>
-			</div><!-- col-lg-4 -->
-
-			<div class="col-lg-4">
-				<i class="fa fa-eye"></i>
-				<h4>Warehousing & Inventory System</h4>
-				<p  class="product_desc">
+                <i class="fa fa-eye"></i>
+                <h4>Warehousing & Inventory System</h4>
+                <p  class="product_desc">
                     Centralized management of warehousing activities includes inventory control, tracking, and the location of stock items.
-				</p>
-			</div><!-- col-lg-4 -->
-		</div><!-- row -->
-		<br>
-		<div class="row centered">
-			<br><br>
+                </p>
+
+                <i class="fa fa-cloud"></i>
+                <h4>Mobile Read & Bill System</h4>
+                <p class="product_desc">Suitable for Water and Electric Industries. Allow meter readers to read water and electric consumption and print bill right away using mobile devices.</p>
+
+
+            </div><!-- col-lg-4 -->
+
 			<div class="col-lg-4">
-				<i class="fa fa-heart"></i>
-				<h4>System for Water & Electric Utilities</h4>
-				<p class="product_desc">
+                <i class="fa fa-heart"></i>
+                <h4>System for Water & Electric Utilities</h4>
+                <p class="product_desc">
                     Comprehensive, enhanced and mature products of Tri-Nvent with the option to choose between web based or desktop applications made of Java and Vb.Net.  Functions like rate management is dynamic, any rate can be added automatically without programming and report modifications.
                 </p>
+
+                <i class="fa fa-shopping-cart"></i>
+                <h4>Queueing  System</h4>
+                <p class="product_desc">Highly customizable software that enable you to eliminate chaos at payment counters.</p>
+
+                <i class="fa fa-cloud"></i>
+                <h4>Point of Sale & Inventory System</h4>
+                <p class="product_desc">Tailored to cater wide range of industries. Bundled with reports and charts that could be helpful in decision making.</p>
+
+
 			</div><!-- col-lg-4 -->
 
 			<div class="col-lg-4">
-				<i class="fa fa-shopping-cart"></i>
-				<h4>Queueing  System</h4>
-				<p class="product_desc">Highly customizable software that enable you to eliminate chaos at payment counters.</p>
-			</div><!-- col-lg-4 -->
+                <i class="fa fa-cogs"></i>
+                <h4>Human Resource & Payroll System</h4>
+                <p class="product_desc">
+                    The system is deployed to company users with enable modules according to their uses. The software let everybody keep tracks of their individual records like leaves, absences, attendance, overtime, leave balances, etc.  They can process overtime, leave, failure to logs, etc. online without disturbing HR personnel.
+                </p>
 
-			<div class="col-lg-4">
-				<i class="fa fa-cloud"></i>
-				<h4>Mobile Read & Bill System</h4>
-				<p class="product_desc">Suitable for Water and Electric Industries. Allow meter readers to read water and electric consumption and print bill right away using mobile devices.</p>
-			</div><!-- col-lg-4 -->
-		</div><!-- row -->
-        <div class="row centered">
-            <br><br>
-            <div class="col-lg-4">
+                <i class="fa fa-shopping-cart"></i>
+                <h4>SMS Custom applications</h4>
+                <p class="product_desc">Designed to connect to any databases and enable your business to broadcast messages (e.g. announcements) to the clients.</p>
+
                 <i class="fa fa-cloud"></i>
                 <h4>Membership System for Electric Utilities</h4>
                 <p class="product_desc">Enable you to manage applications and members. Printing of members' ID is also supported.</p>
             </div><!-- col-lg-4 -->
+		</div><!-- row -->
 
-            <div class="col-lg-4">
-                <i class="fa fa-shopping-cart"></i>
-                <h4>SMS Custom applications</h4>
-                <p class="product_desc">Designed to connect to any databases and enable your business to broadcast messages (e.g. announcements) to the clients.</p>
-            </div><!-- col-lg-4 -->
-
-            <div class="col-lg-4">
-                <i class="fa fa-cloud"></i>
-                <h4>Point of Sale & Inventory System</h4>
-                <p class="product_desc">Tailored to cater wide range of industries. Bundled with reports and charts that could be helpful in decision making.</p>
-            </div><!-- col-lg-4 -->
-        </div><!-- row -->
+        <div class="row centered">
+            <h3>What else we can offer?</h3>
+            <div class="col-lg-10 col-lg-offset-3" style="text-align: left">
+                <br/>
+                <div>
+                    <p><i style="font-size: 16px;" class="fa fa-desktop"></i><strong>&nbsp;&nbsp;Web Design and Development</strong> – Concrete5 and WordPress</p>
+                    <p><i style="font-size: 16px;" class="fa fa-desktop"></i><strong>&nbsp;&nbsp;Computer Networks</strong> – Design and Implementation of Wireless and LAN </p>
+                    <p><i style="font-size: 16px;"class="fa fa-print"></i><strong>&nbsp;&nbsp;Epson Printer Products</strong> – We are EPSON partner</p>
+                    <p><i style="font-size: 16px;" class="fa fa-desktop"></i><strong>&nbsp;&nbsp;Transactional Web Applications Development</strong> – Java, PHP and .Net</p>
+                </div>
+            </div>
+        </div>
 	</div><!-- container -->
 
 <?php $this->inc('elements/footer.php'); ?>
