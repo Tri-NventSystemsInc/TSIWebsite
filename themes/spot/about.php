6035c9c0d12c16d15e54b0880231611c68e7e93c
Arjay Adong <arjayads@gmail.com>
2014-09-02 11:10:30 +0800

    profile updates

--- themes/spot/about.php
+++ themes/spot/about.php
@@ -1,10 +1,21 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
+    <style>
+        p.profile {
+            font-size: 16px;
+            text-align: justify;
+        }
+        h5.title {
+            color: #f85c37;
+            font-size: 13px;
+        }
+    </style>
 
-	<div id="blue">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-10 col-lg-offset-1">
+
+    <div id="blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1">
                     <h4>What is Tri-nvent...</h4>
                     <p>Tri-Nvent is a software development company based in Dumaguete City, Philippines thriving to use the recent state-of-the-art technology in the IT industry like Java and .net platforms, agile technology workflow, mobile technology, and many others.</p>
 
@@ -18,79 +29,84 @@ $this->inc('elements/header.php'); ?>
                         <i class="fa fa-user"></i>&nbsp;&nbsp;Relationship driven approach between Stakeholders, employee, and clients<br>
                         <i class="fa fa-road"></i>&nbsp;Test driven development
                     </p>
-				</div>
-			</div><!-- row -->
-		</div><!-- container -->
-	</div><!--  bluewrap -->
+                </div>
+            </div><!-- row -->
+        </div><!-- container -->
+    </div><!--  bluewrap -->
 
 
-	<div class="container w">
-		<div class="row centered">
-			<br><br>
-			<div class="col-lg-3">
-				<img class="img-circle" src="/files/3014/0826/0613/volt.JPG" width="110" height="130" alt="">
-				<h4>Voltaire Picardo</h4>
-				<h5 style="color: #f85c37">CEO, Founder</h5>
-				<p>Voltaire founded Tri-nvent Systems Inc. in 2009 and manages the company since its establishment up to the present.
+    <div class="container w">
+        <div class="row centered">
+            <br><br>
+            <div class="col-lg-3">
+                <img class="img-circle" src="/files/3014/0826/0613/volt.JPG" width="110" height="130" alt="">
+                <h4>Voltaire Picardo</h4>
+                <h5 class="title">CEO, Founder</h5>
+                <p class="profile">Voltaire founded Tri-nvent Systems Inc. in 2009 and manages the company since its establishment up to the present.
                     He specializes in software designs and development.
                     His over 10 years of programming experience honed him to become an expert in various databases and development tools.</p>
-			</div><!-- col-lg-3 -->
+            </div><!-- col-lg-3 -->
 
-			<div class="col-lg-3">
-				<img class="img-circle" src="/files/9314/0826/0896/rey.JPG" width="110" height="130" alt="">
-				<h4>Rey Repe</h4>
-                <h5 style="color: #f85c37">Co-Founder</h5>
-				<p>Rey is a visionary, an extremely talented individual with over a decade of industry experience in both hardware and software.
+            <div class="col-lg-3">
+                <img class="img-circle" src="/files/9314/0826/0896/rey.JPG" width="110" height="130" alt="">
+                <h4>Rey Repe</h4>
+                <h5 class="title">Co-Founder</h5>
+                <p class="profile">Rey is a visionary, an extremely talented individual with over a decade of industry experience in both hardware and software.
                     Presently, he is a MIS Head of a prestigious Utility Company in the country.
                     His expertise covers systems analysis and development, networks and server administration.</p>
-			</div><!-- col-lg-3 -->
+            </div><!-- col-lg-3 -->
 
-			<div class="col-lg-3">
-				<img class="img-circle" src="/files/2514/0826/1661/ramil.JPG" width="110" height="130" alt="">
-				<h4>Ramil Repe, CPA</h4>
-                <h5 style="color: #f85c37">Co-Founder</h5>
-				<p>Ramil started out as an Auditor in trusted and prominent auditing firms in the country.
+            <div class="col-lg-3">
+                <img class="img-circle" src="/files/2514/0826/1661/ramil.JPG" width="110" height="130" alt="">
+                <h4>Ramil Repe, CPA</h4>
+                <h5 class="title">Co-Founder</h5>
+                <p class="profile">Ramil started out as an Auditor in trusted and prominent auditing firms in the country.
                     His brilliance and superb leadership skill pave the way to his appointment as an office manager in a famous credit cooperative in the Philippines.
                     He is responsible for the development of Tri-Nvent’s Accounting System.</p>
-			</div><!-- col-lg-3 -->
+            </div><!-- col-lg-3 -->
 
-			<div class="col-lg-3">
-				<img class="img-circle" src="/files/5314/0826/2233/mikho1.JPG" width="110" height="130" alt="">
-				<h4>Mikho Anthony Malto</h4>
-				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
-			</div><!-- col-lg-3 -->
+            <div class="col-lg-3">
+                <img class="img-circle" src="/files/5314/0826/2233/mikho1.JPG" width="110" height="130" alt="">
+                <h4>Mikho Anthony Malto</h4>
+                <h5 class="title">Software Engineer</h5>
+                <p class="profile">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+            </div><!-- col-lg-3 -->
 
-		</div><!-- row -->
+        </div>
 
         <div class="row centered">
             <br><br>
             <div class="col-lg-3">
                 <img class="img-circle" src="/files/5214/0826/6471/tonyo.jpg" width="110" height="130" alt="">
                 <h4>Tony Anthony Zerna</h4>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+                <h5 class="title">Software Engineer</h5>
+                <p class="profile">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
             </div><!-- col-lg-3 -->
 
             <div class="col-lg-3">
                 <img class="img-circle" src="/files/3014/0826/5283/arjay.jpg" width="110" height="130" alt="">
                 <h4>Arjay Adong</h4>
-                <p>Arjay is a true hard core software developer. For best results give him the freedom to show you what he can do and allow him to be creative. A true professional, he’ll deliver more than you expected. Guaranteed.</p>
+                <h5 class="title">Software Engineer</h5>
+                <p class="profile">Arjay is a true hard core software developer. For best results give him the freedom to show you what he can do and allow him to be creative. A true professional, he’ll deliver more than you expected. Guaranteed.</p>
             </div><!-- col-lg-3 -->
 
             <div class="col-lg-3">
                 <img class="img-circle" src="/files/5314/0829/1075/lester.jpg" width="110" height="130" alt="">
                 <h4>Lester John Paul Cadiz</h4>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+                <h5 class="title">Software Engineer / Designer</h5>
+                <p class="profile">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
             </div><!-- col-lg-3 -->
 
             <div class="col-lg-3">
                 <img class="img-circle" src="/files/7814/0829/1446/ryan.png" width="110" height="130" alt="">
                 <h4>Ryan Repe</h4>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+                <h5 class="title">Software Engineer</h5>
+                <p class="profile">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
             </div><!-- col-lg-3 -->
 
         </div>
-		<br>
-		<br>
-	</div><!-- container -->
+        <br>
+        <br>
+    </div><!-- container -->
 
 <?php $this->inc('elements/footer.php'); ?>
